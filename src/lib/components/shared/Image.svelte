e6805a347b41c9a8437941a32c82dc6b961c9ebe
nicozygo <zygos.nico@gmail.com>
2023-03-09 15:51:15 +0000

    add async image compoment

---
+++ src/lib/components/shared/Image.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import { onMount, onDestroy } from 'svelte';
+	let src: string;
+	export let url: string;
+	export let alt: string = '';
+	export let width: string = '100%';
+	export let height: string = '100%';
+	export let style: string = '';
+
+	onMount(async () => {
+		const res = await fetch(url);
+		src = await res.url;
+	});
+
+	onDestroy(() => {
+		src = '';
+		url = '';
+	});
+</script>
+
+{#if src}
+	<img {src} {alt} {width} {height} {style} />
+{/if}
