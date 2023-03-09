772a6030a44d9a0ebbe2187924daa4803dfbcaff
nicozygo <zygos.nico@gmail.com>
2023-03-09 22:01:22 +0000

    add dark mode switch

--- src/lib/components/homepage/Main.svelte
+++ src/lib/components/homepage/Main.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import ScrollButton from '$shared/ScrollButton.svelte';
+	import DarkSwitch from '$shared/DarkSwitch.svelte';
 
 	export let width: number;
 	let main: HTMLElement | null;
@@ -26,6 +27,9 @@
 	<slot />
 </main>
 <div class="row">
+	<div class="mode">
+		<DarkSwitch />
+	</div>
 	<div class="scrolling">
 		<ScrollButton />
 	</div>
@@ -45,6 +49,20 @@
 	main::-webkit-scrollbar {
 		display: none;
 	}
+
+	.mode,
+	.scrolling {
+		position: absolute;
+		bottom: 5%;
+	}
+
+	.mode {
+		left: 2%;
+	}
+
+	.scrolling {
+		right: 2%;
+	}
 	@media (max-width: 1024px) {
 		main {
 			flex-wrap: wrap;
@@ -52,13 +70,7 @@
 			overflow-x: hidden;
 			height: auto;
 		}
-	}
-	.scrolling {
-		position: absolute;
-		bottom: 5%;
-		right: 2%;
-	}
-	@media (max-width: 1024px) {
+		.mode,
 		.scrolling {
 			display: none;
 		}
---
+++ src/lib/components/shared/DarkSwitch.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+	import Icon from './Icon.svelte';
+	let dark = false;
+</script>
+
+<button on:click={() => (dark = !dark)} class:active={dark}>
+	{#if dark}
+		<Icon name="sun" size="50px" />
+		<span>Light Mode</span>
+	{:else}
+		<Icon name="moon" size="50px" />
+		<span>Dark Mode</span>
+	{/if}
+</button>
+
+<style>
+	button {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		text-decoration: none;
+		border: 0;
+		transition: all 0.5s ease-in-out;
+	}
+	.active {
+		transition: all 0.5s ease-in-out;
+	}
+
+	span {
+		font-family: var(--heading-font);
+		text-transform: uppercase;
+		font-weight: 600;
+		font-size: 1.5rem;
+		padding: var(--base-padding);
+	}
+</style>
