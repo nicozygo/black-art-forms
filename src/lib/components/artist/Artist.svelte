724953aa76bfc34fbf1d0457946f7f0ab19a6051
nicozygo <zygos.nico@gmail.com>
2023-03-10 14:27:05 +0000

    create artist page + artist component

---
+++ src/lib/components/artist/Artist.svelte
@@ -0,0 +1,51 @@
+<script lang="ts">
+	import SvelteMarkdown from 'svelte-markdown';
+	import Image from '$shared/Image.svelte';
+	export let name: string;
+	export let bio: string;
+	export let profile: { alternativeText: string; url: string; caption: string; name: string };
+</script>
+
+<section class="vertical">
+	<div class="row container">
+		<div class="profile">
+			<div class="diamond">
+				<div class="inner">
+					<Image url={profile.url} alt={profile.alternativeText} class="photo"/>
+				</div>
+			</div>
+		</div>
+		<div class="bio">
+			<h2 class="title mb-2 pr-2">{name}</h2>
+			<SvelteMarkdown source={bio} />
+		</div>
+	</div>
+</section>
+
+<style>
+	.profile {
+		margin: 10px;
+	}
+
+	.diamond {
+		margin: auto;
+		transform-origin: 50% 50%;
+		overflow: hidden;
+		width: 250px;
+		height: 250px;
+		transform: rotate(45deg) translateY(10px);
+		border: var(--base-padding) solid black;
+	}
+		.inner {
+			width: 355px;
+			height: 355px;
+			transform: rotate(-45deg) translateY(-74px);
+		}
+
+		@media screen and (max-width: 1200px) {
+			.profile {
+			margin: 100px auto 100px;
+			}
+		}
+
+</style>
--- src/routes/artists/[slug]/+page.svelte
+++ src/routes/artists/[slug]/+page.svelte
@@ -1,14 +1,29 @@
 <script lang="ts">
-	import type { PageData } from './$types';
+	import type { PageData } from '../../$types';
+	import Artist from '$lib/components/artist/Artist.svelte';
 
 	export let data: PageData;
+	let artist = data.props.artists.data[0].attributes;
+	export let profile = artist.profilePhoto.data.attributes;
+	export let name = artist.fullName;
+	export let bio = artist.description;
 </script>
 
 <main>
-	{JSON.stringify(data)}
-	<p>
-		<a href="/">
-			<button>Go to homepage</button>
-		</a>
-	</p>
+	<Artist {name} {profile} {bio} />
 </main>
+
+<style>
+	main {
+		padding: 0;
+		display: flex;
+		flex-wrap: wrap;
+	}
+
+	@media (max-width: 1024px) {
+		main {
+			flex-direction: column;
+		}
+	}
+
+</style>
