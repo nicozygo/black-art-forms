074b872974a0fd23c609848ac021cd4fd574bab0
nicozygo <zygos.nico@gmail.com>
2023-03-13 10:43:45 +0000

    add homepage artist gallery components

---
+++ src/lib/components/homepage/Artist.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  import ImageHover from '$shared/ImageHover.svelte';
+  import type { ArtistType } from '$types/ArtistType';
+  export let artist: ArtistType;
+  const url = artist.attributes.featuredImage.data.attributes.url;
+  const alt = artist.attributes.featuredImage.data.attributes.alternativeText;
+  const slug = artist.attributes.slug;
+  const name = artist.attributes.fullName;
+  const title = artist.attributes.featuredImage.data.attributes.name;
+</script>
+
+<div class="item">
+  <ImageHover
+    { url } { alt} {name} {title} link={`/artists/${slug}`}
+  />
+</div>
+
+<style>
+  .item {
+		display: inline-block;
+		margin: 0 0 var(--base-margin);
+		width: 100%;
+	}
+</style>
---
+++ src/lib/components/homepage/Artists.svelte
@@ -0,0 +1,61 @@
+<script lang="ts">
+import Artist from './Artist.svelte';
+	export let title: string;
+	export let artists: {
+		id: number;
+		attributes: {
+			fullName: string;
+			excerpt: string;
+			slug: string;
+			genre: string;
+			featured: boolean;
+			featuredImage: {
+				data: {
+					attributes: { name: string; alternativeText: string; caption: string; url: string };
+				};
+			};
+		};
+	}[];
+	export let open: boolean;
+</script>
+
+<section
+	class="horizontal display-f justify-center align-center pt-2"
+	id={title.toLocaleLowerCase()}
+>
+	<div class="row">
+			<div class="row">
+				<div class="masonry" style={open ? 'column-count:6' : 'column-count:5'}>
+					{#each artists as artist}
+						<Artist {artist} />
+					{/each}
+				</div>
+			</div>
+		</div>
+</section>
+
+<style>
+	.masonry {
+		column-gap: var(--base-padding);
+		transition-property: all 4s ease-in-out 1s;
+	}
+
+	@media only screen and (max-width: 1023px) and (min-width: 768px) {
+		.masonry {
+			column-count: 4;
+		}
+		}
+
+		@media only screen and (max-width: 767px) and (min-width: 480px) {
+			.masonry {
+			column-count: 2;
+			}
+		}
+
+		@media only screen and (max-width: 479px) {
+			.masonry {
+			column-count: 2;
+			}
+		}
+
+</style>
